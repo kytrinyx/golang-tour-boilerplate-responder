a14ce46d7079066b3ce0823392fe81887d46a99b
Katrina Owen <kytrinyx@github.com>
2017-06-19 06:02:49 -0600

    Upgrade probot to v0.7.0

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function(robot) {
-  robot.on('issues.opened', async (event,  context) => {
-    const title = event.payload.issue.title;
+  robot.on('issues.opened', async (context) => {
+    const title = context.payload.issue.title;
     const rgxTitle = /[REPLACE WITH SHORT DESCRIPTION]/;
 
-    const body = event.payload.issue.body;
+    const body = context.payload.issue.body;
     const rgxBody = /Change the title above to describe your issue and add your feedback here, including code if necessary/;
 
-    const cannedReply = "Please edit the title and body of your issue to describe your problem. Be sure to delete the boilerplate. Once you've done this, you may reopen the issue.";
+    const cannedReply = "Thanks for your issue report, but unfortunately you haven't told us what the problem was. Please edit the title and body of your issue to describe your problem. Be sure to delete the boilerplate. Once you've done this, you may reopen the issue.";
 
     if (title.match(rgxTitle) || body.match(rgxBody)) {
       return context.github.issues.edit(context.issue({state: 'closed'})).then(() => {
